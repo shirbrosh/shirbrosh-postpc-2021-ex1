072a74db8c10aabcc9b7dd1b8dc50e530c80b562
shirbrosh <shir.brosh@mail.huji.ac.il>
2021-03-29 20:56:21 +0300

    finish with animations TODO: work on the keyboard

--- app/src/main/java/android/exercise/mini/interactions/EditTitleActivity.java
+++ app/src/main/java/android/exercise/mini/interactions/EditTitleActivity.java
@@ -1,7 +1,9 @@
 package android.exercise.mini.interactions;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,15 +14,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditTitleActivity extends AppCompatActivity {
 
-  // TODO:
-  //  you can add fields to this class. those fields will be accessibly inside any method
-  //  (like `onCreate()` and `onBackPressed()` methods)
-  // for any field, make sure to set it's initial value. You CAN'T write a custom constructor
-  // for example, you can add this field:
-  // `private boolean isEditing = false;`
-  // in onCreate() set `this.isEditing` to `true` once the user starts editing, set to `false` once done editing
-  // in onBackPressed() check `if(this.isEditing)` to understand what to do
 
+  private boolean isEditing = false;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -42,6 +37,38 @@ public class EditTitleActivity extends AppCompatActivity {
 
     // handle clicks on "start edit"
     fabStartEdit.setOnClickListener(v -> {
+      this.isEditing = true;
+
+      // animate out the "start edit" FAB
+      fabStartEdit.animate()
+              .alpha(0f)
+              .setDuration(500L)
+              .withEndAction(() -> fabStartEdit.setVisibility(View.GONE))
+              .start();
+
+      // animate in the "done edit" FAB
+      fabEditDone.setVisibility(View.VISIBLE);
+      fabEditDone.setAlpha(0f);
+      fabEditDone.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+
+      // disappear the text view and animate in the edit title
+      textViewTitle.setVisibility(View.GONE);
+
+      editTextTitle.setVisibility(View.VISIBLE);
+      editTextTitle.setAlpha(0f);
+      editTextTitle.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+//      editTextTitle.requestFocus();
+
+//      InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//      imm.hideSoftInputFromWindow(textViewTitle.getWindowToken(), 0);
+
+
       /*
       TODO:
       1. animate out the "start edit" FAB
@@ -58,6 +85,38 @@ public class EditTitleActivity extends AppCompatActivity {
 
     // handle clicks on "done edit"
     fabEditDone.setOnClickListener(v -> {
+      this.isEditing = false;
+
+      // animate out the "done edit" FAB
+      fabEditDone.animate()
+              .alpha(0f)
+              .setDuration(500L)
+              .withEndAction(() -> fabEditDone.setVisibility(View.GONE))
+              .start();
+
+      // animate in the "start edit" FAB
+      fabStartEdit.setVisibility(View.VISIBLE);
+      fabStartEdit.setAlpha(0f);
+      fabStartEdit.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+
+      // save the new edited text
+      String newText = editTextTitle.getText().toString();
+      editTextTitle.setText(newText);
+      textViewTitle.setText(newText);
+
+      // disappear the edit title and animate in the text view
+      editTextTitle.setVisibility(View.GONE);
+
+      textViewTitle.setVisibility(View.VISIBLE);
+      textViewTitle.setAlpha(0f);
+      textViewTitle.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+
       /*
       TODO:
       1. animate out the "done edit" FAB
@@ -74,6 +133,45 @@ public class EditTitleActivity extends AppCompatActivity {
 
   @Override
   public void onBackPressed() {
+    // find all views
+    FloatingActionButton fabStartEdit = findViewById(R.id.fab_start_edit);
+    FloatingActionButton fabEditDone = findViewById(R.id.fab_edit_done);
+    TextView textViewTitle = findViewById(R.id.textViewPageTitle);
+    EditText editTextTitle = findViewById(R.id.editTextPageTitle);
+
+    if(this.isEditing){
+
+      // save the edit text title as the old title before the change
+      String oldText = textViewTitle.getText().toString();
+      editTextTitle.setText(oldText);
+
+      // disappear the edit title and animate in the text view
+      editTextTitle.setVisibility(View.GONE);
+      textViewTitle.setVisibility(View.VISIBLE);
+      textViewTitle.setAlpha(0f);
+      textViewTitle.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+
+      // animate out the "done-edit" FAB
+      fabEditDone.animate()
+              .alpha(0f)
+              .setDuration(500L)
+              .withEndAction(() -> fabEditDone.setVisibility(View.GONE))
+              .start();
+
+      // animate in the "start-edit" FAB
+      fabStartEdit.setVisibility(View.VISIBLE);
+      fabStartEdit.setAlpha(0f);
+      fabStartEdit.animate()
+              .alpha(1f)
+              .setDuration(500L)
+              .start();
+    }
+    else{
+      super.onBackPressed();
+    }
     // BACK button was clicked
     /*
     TODO:
